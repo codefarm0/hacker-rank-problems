65c3e821ffc46a5ece7fe5ae8175921a82e86cfc
Green Learner <greenlearner01@users.noreply.github.com>
2020-02-26 11:33:01 +0530

    magic square

--- src/com/greenlearner/hackerrank/problemsolving/FormingAMagicSquare.java
+++ src/com/greenlearner/hackerrank/problemsolving/FormingAMagicSquare.java
@@ -10,8 +10,27 @@ public class FormingAMagicSquare {
     // Complete the formingMagicSquare function below.
     static int formingMagicSquare(int[][] s) {
 
-        //TODO
-        return 0;
+        int n = 3;
+        int cost = Integer.MAX_VALUE;
+
+        // all possible combination of magic arrays
+        int[][][] allMagicSquares = {{{8, 1, 6}, {3, 5, 7}, {4, 9, 2}}, {{6, 1, 8}, {7, 5, 3}, {2, 9, 4}},
+                {{8, 3, 4}, {1, 5, 9}, {6, 7, 2}}, {{6, 7, 2}, {1, 5, 9}, {8, 3, 4}},
+                {{4, 9, 2}, {3, 5, 7}, {8, 1, 6}}, {{2, 9, 4}, {7, 5, 3}, {6, 1, 8}},
+                {{2, 7, 6}, {9, 5, 1}, {4, 3, 8}}, {{4, 3, 8}, {9, 5, 1}, {2, 7, 6}}};
+
+        for (int i = 0; i < n * n - 1; i++) {
+            int difference = 0;
+            for (int x = 0; x < n; x++) {
+                for (int y = 0; y < n; y++) {
+                    difference += Math.abs(allMagicSquares[i][x][y] - s[x][y]);
+                }
+            }
+            if (difference < cost)
+                cost = difference;
+        }
+
+        return cost;
     }
 
     private static final Scanner scanner = new Scanner(System.in);

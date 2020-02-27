4479bd3f16c326943928eaaf446bd0978b110051
Green Learner <greenlearner01@users.noreply.github.com>
2020-02-27 09:14:20 +0530

    picking a number

---
+++ src/com/greenlearner/hackerrank/problemsolving/PickingNumbers.java
@@ -0,0 +1,62 @@
+package com.greenlearner.hackerrank.problemsolving;
+
+import java.io.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * @author - GreenLearner(https://www.youtube.com/c/greenlearner)
+ */
+class Result {
+
+    /*
+     * Complete the 'pickingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int pickingNumbers(List<Integer> a) {
+        Collections.sort(a);//sort the list
+        int start = 0;
+        int index = 0;
+        int max = 0;
+        while (index < a.size()) {
+            if (Math.abs(a.get(start) - a.get(index)) > 1) {
+                start = index;
+            }
+            max = Math.max(max, index - start + 1);
+            index++;
+        }
+        index--;
+        int maxLength = Math.max(max, index - start + 1);
+        return maxLength;
+    }
+
+}
+
+}
+
+public class PickingNumbers {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.pickingNumbers(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
